e23ddb8fecd9a5b66844dc4c0a92b2c2fb37c17e
Ryan Desmond <ryan@codingnomads.co>
2021-03-09 18:05:22 -0800

    first push of sample Spring MVC app

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sample Comedy App with Spring MVC
+
+This is a sample application used in CodingNomads [Advanced Java and Spring Framework course](https://platform.codingnomads.co/learn/).
+
+## Running the App
+
+If you'd like to give it a try on your own first, follow these steps:
+
+* Clone this project
+* Open this project in IntelliJ as a Maven project
+* Locate the `ComedyApplication` class
+* Right click on the `ComedyApplication` class and select "Run
+  ComedyApplication" (exact language of the run options may vary between
+  IDEs and versions.)
+* Open a web browser of your choice and hit this url <a
+  href='http://localhost:8080/comedy'
+  target='_blank'>http://localhost:8080/comedy</a>
+
+## License
+See license.txt
---
+++ src/main/java/demo/ComedyController.java
@@ -0,0 +1,37 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *  Joke Sources:
+ *  	* https://www.rd.com/list/short-jokes/
+ *  	* https://devdojo.com/devdojo/10-awesome-programming-jokes
+ *  	* https://levelup.gitconnected.com/10-programming-jokes-for-programmers-b9c436f3e023
+ *		* https://www.hongkiat.com/blog/programming-jokes/
+ */
+
+// The @Controller annotation indicates that this class is a web service
+@Controller
+public class ComedyController {
+
+    // The line below "injects" a bean of "demo.ComedyService" into this class
+    @Autowired
+    ComedyService service;
+
+    // This method will be invoked when the baseUrl + "/comedy" is hit
+	@GetMapping("/comedy")
+	public String greeting (Model model) {
+	    // Get a random joke from the service
+		String joke = service.getRandomJoke();
+
+		// Attach that joke to the data model
+		model.addAttribute("joke", joke);
+
+		// Return the view "getting_started/comedy.html"
+        // this view can be found at "src/main/resources/templates/getting_started/comedy.html
+		return "getting_started/comedy";
+	}
+}
